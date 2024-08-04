5d43eaa89a1829bdb7074c721a30b750eacef7da
nguyenducdung <dunggs2020@gmail.com>
2024-08-04 17:08:04 +0700

    push by dung

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
 
+import './App.scss';
+
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ứng dụng React đầu tiên của dùng dành cho vợ Loan hehe
-        </p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
+      Loan oi doi a nhe
     </div>
   );
 }
